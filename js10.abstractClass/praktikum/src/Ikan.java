d2a56b59277d80cffbc8acdfdc43f7ddadad5933
nandashabrina <shabrina2604@gmail.com>
2022-11-18 09:32:45 +0700

    Commit Jobsheet 10 - Abstract Class

---
+++ js10.abstractClass/praktikum/src/Ikan.java
@@ -0,0 +1,7 @@
+public class Ikan extends Hewan{
+    public abstract void bergerak();
+    // @Override
+    // public void bergerak(){
+    //     System.out.println("Berenang dengan sirip, \"wush..wush..\"");
+    // }
+}
---
+++ js10.abstractClass/praktikum/src/Kucing.java
@@ -0,0 +1,6 @@
+public class Kucing extends Hewan{
+    @Override
+    public void bergerak(){
+       System.out.println("Berjalan dengan kaki, \"tap..tap..\"");
+    }
+}
